df9c793f777e482d4b8234049c1d6bdb913b5cff
limyel <limyel@outlook.com>
2025-07-15 17:15:33 +0800

    feat(bst): 实现二叉搜索树的基本功能
    - 新增 Node 结构体和相关方法- 实现 BST 结构体的初始化、插入和搜索功能
    - 优化代码结构，提高可读性和可维护性

--- ds/bst/bst.go
+++ ds/bst/bst.go
@@ -1,19 +1,69 @@
 package bst
 
-import "github.com/limyel/base/ds"
+import (
+	"github.com/limyel/base/ds"
+)
 
-type node struct {
+type Node struct {
 	value ds.Comparable
-	left  *node
-	right *node
+	left  *Node
+	right *Node
 }
 
 // BST 二叉查找树
 // 对于任意节点 N，左节点的值小于 N，右节点的值大于 N
 type BST struct {
-	Root *node
+	root *Node
+}
+
+func newNode(value ds.Comparable) *Node {
+	return &Node{
+		value: value,
+		left:  nil,
+		right: nil,
+	}
+}
+
+func NewBST() *BST {
+	return &BST{
+		root: nil,
+	}
+}
+
+func (bst *BST) Search(value ds.Comparable) bool {
+	return doSearch(bst.root, value)
+}
+
+func doSearch(node *Node, value ds.Comparable) bool {
+	if node == nil {
+		return false
+	}
+
+	if node.value.Compare(value) == 0 {
+		return true
+	}
+
+	if value.Compare(node.value) < 0 {
+		return doSearch(node.left, value)
+	} else {
+		return doSearch(node.right, value)
+	}
 }
 
 func (bst *BST) Insert(value ds.Comparable) {
+	bst.root = doInsert(bst.root, value)
+}
 
+func doInsert(node *Node, value ds.Comparable) *Node {
+	if node == nil {
+		return newNode(value)
+	}
+
+	if value.Compare(node.value) > 0 {
+		node.right = doInsert(node.right, value)
+	} else if value.Compare(node.value) < 0 {
+		node.left = doInsert(node.left, value)
+	}
+
+	return node
 }
